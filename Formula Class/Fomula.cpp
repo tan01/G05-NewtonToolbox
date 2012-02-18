87fc50010ea9fc2d6d6896507aac401905c04058
tan01 <fenrir754@gmail.com>
2012-02-18 11:01:38 -0800

    Deleting the last remnants of C++. We're doing this in java, I guess.

--- Formula Class/Fomula.cpp
+++
@@ -1,9 +0,0 @@
-// Formula.cpp
-// Clayven Anderson 2/3/12
-// Implementation to of the Formual class (Obviously not done yet) 
-
-#include "Formula.h"
-#include <string>
-using namespace std;
-
-// IMPLEMENT Constructor, Destructor and methods.
