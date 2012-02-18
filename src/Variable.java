229fe38e83d089b20b02fae9b53dbb6cf7ca78af
unknown <My Lynn@MayFast.(none)>
2012-02-18 10:52:38 -0800

    added a value instance variable, get/set methods for it, and a constructor that takes char var and double value
    need a way to store a value inside a variable

--- src/Variable.java
+++ src/Variable.java
@@ -1,33 +1,62 @@
 /** Variable is a class to represent a variable
 	@author Michelle Len
+	@author May Camp
 	@version 02/14/2012 for CS 48 Project, W12
 */
 
 public class Variable {
-
+	private double value; //value of the variable
 	private char var; // variable represented as a char
 
-	/** Constructor
-		@param var What the variable is
-	*/
+	/** Constructor	
+	 * @param var A character of what the variable is
+	 */
 
 	public Variable (char var) {
 		// assign attributes from parameters
 		this.var = var;
 	}
 
+	/** Constructor
+	 * 
+	 * @param var A character of what the variable is
+	 * @param value A double of what the value of the variable is
+	 */
+	public Variable (char var, double value){
+		this.var = var;
+		this.value = value;
+	}
+	
 	/**
 		Get the variable character
  	*/
-	
 	public char getVar () {
 		return this.var;
+	}
+	/**
+	 * Get the variable value
+	 */
+	public double getValue() {
+		return this.value;
+	}
+	
+	/**
+	 * set the variable character
+	 */
+	public void setVar (char var) {
+		this.var = var;
+	}
+	
+	/**
+	 * set the variable value
+	 */
+	public void setValue (double value) {
+		this.value = value;
 	}
 
 	/**
 		Returns object as a string
 	*/
-	
 	public String toString() {
 		return Character.toString(var);
 	} // do we need this method?
