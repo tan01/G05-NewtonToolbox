b7164ee2f76ebe1f91327170f16fe14bca7c94e0
Michelle Len <mlen@umail.ucsb.edu>
2012-02-19 12:43:40 -0800

    Created an Operator class.
    Signed-off-by: Michelle Len <mlen@umail.ucsb.edu>

---
+++ src/internalformatting/Operator.java
@@ -0,0 +1,46 @@
+package internalformatting;
+
+/** Operator is a class to represent an operator such as (, ), +, -, *, /
+ * @author Michelle Len
+ * @version 02/19/2012 for CS 48 Project, W12
+ */
+
+public class Operator extends Term {
+	
+	private String operator;
+	private static final long serialVersionUID =  1767185L;
+	
+	public Operator() {
+		super();
+	}
+	
+	/** Constructor
+	 * 
+	 * @param operator A String of the operator
+	 */
+	
+	public Operator(String operator) {
+		this.operator = operator;
+	}
+
+	/**
+	 * Get method for operator
+	 */
+	
+	public String getOperator() { return this.operator; }
+	
+	/** 
+	 * Set method for operator
+	 */
+	
+	public void setOperator(String operator) { this.operator = operator; }
+	
+	/**
+	 * Returns object as a string
+	 */
+	
+	public String toString() {
+		return operator;
+	}
+	
+} // class Operator
