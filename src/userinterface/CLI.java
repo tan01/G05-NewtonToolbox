b61249a9c4a232c6030bb5027f4a046de2e07203
unknown <My Lynn@MayFast.(none)>
2012-02-19 13:39:00 -0800

    indent

--- src/userinterface/CLI.java
+++ src/userinterface/CLI.java
@@ -14,86 +14,86 @@ CLI.java
 User interface for the sake of interfacing with users.
 @author Jonathan Tan
 @version 0.0.1 2/18/12
-*/
+ */
 
 public class CLI
 {
-static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 
-//Valid search terms go here.
-//May end up making a new class for this thing,
-//Need a smarter way of checking if the term is in the array.
-//Maybe ArrayList?
-//Need Array "find" method!
+	//Valid search terms go here.
+	//May end up making a new class for this thing,
+	//Need a smarter way of checking if the term is in the array.
+	//Maybe ArrayList?
+	//Need Array "find" method!
 
-static String[] comsearch = new String[]{"s","search"};
-static String[] comquit = new String[]{"exit","q","quit",};
-static String[] comprint = new String[]{"p","print"};
-static String[] comprintall = new String[]{"print all","p all", "p a"};
+	static String[] comsearch = new String[]{"s","search"};
+	static String[] comquit = new String[]{"exit","q","quit",};
+	static String[] comprint = new String[]{"p","print"};
+	static String[] comprintall = new String[]{"print all","p all", "p a"};
 
-static ArrayList<Formula> defaultFormulas = new ArrayList<Formula>();
-//STEALING MAY'S CODE
-
-
-public static boolean inArray(String prompt, String[] comArray){
-//I know, I know, I should use a binary search algorithm here.
-//Will improve efficiency in a little bit, okay?
-int truthvalue = Arrays.binarySearch(comArray, prompt);
-if(truthvalue < 0)
-return false;
-else
-return true;
-}
-
-public static void main(String[] args) throws IOException{
-
-String uinput = "";
-while(!uinput.equals("exit") && !uinput.equals("quit")){
-//System.out.println("Command?");
-System.out.print(">");
-uinput = in.readLine().toLowerCase();
-
-//Search case
-if(inArray(uinput,comsearch)){
-System.out.println("Enter search term:");
-System.out.print(">");
-uinput = in.readLine().toLowerCase();
-Search usearch = new Search(defaultFormulas);
-
-ArrayList<Formula> somelist = usearch.searchF(uinput);
-System.out.println("Searching " + "\"" + uinput + "\"...");
-String Aformula = "";
-
-if(somelist.size()<=0)
-System.out.println("No formula found!");
-else{
-
-for(int i = 0;i<somelist.size();i++){
-Aformula = somelist.get(i).toString();
-System.out.println(Aformula);
-}
-
-}
-}
+	static ArrayList<Formula> defaultFormulas = new ArrayList<Formula>();
+	//STEALING MAY'S CODE
 
 
-//Print case
-if(inArray(uinput,comprint)){
-//STUB
-break;
-}
+	public static boolean inArray(String prompt, String[] comArray){
+		//I know, I know, I should use a binary search algorithm here.
+		//Will improve efficiency in a little bit, okay?
+		int truthvalue = Arrays.binarySearch(comArray, prompt);
+		if(truthvalue < 0)
+			return false;
+		else
+			return true;
+	}
 
-if(inArray(uinput,comprintall)){
-//STUB
-break;
-}
+	public static void main(String[] args) throws IOException{
 
-//Quit case
-if(inArray(uinput,comquit)){
-break;
-}
+		String uinput = "";
+		while(!uinput.equals("exit") && !uinput.equals("quit")){
+			//System.out.println("Command?");
+			System.out.print(">");
+			uinput = in.readLine().toLowerCase();
 
-}
-}
+			//Search case
+			if(inArray(uinput,comsearch)){
+				System.out.println("Enter search term:");
+				System.out.print(">");
+				uinput = in.readLine().toLowerCase();
+				Search usearch = new Search(defaultFormulas);
+
+				ArrayList<Formula> somelist = usearch.searchF(uinput);
+				System.out.println("Searching " + "\"" + uinput + "\"...");
+				String Aformula = "";
+
+				if(somelist.size()<=0)
+					System.out.println("No formula found!");
+				else{
+
+					for(int i = 0;i<somelist.size();i++){
+						Aformula = somelist.get(i).toString();
+						System.out.println(Aformula);
+					}
+
+				}
+			}
+
+
+			//Print case
+			if(inArray(uinput,comprint)){
+				//STUB
+				break;
+			}
+
+			if(inArray(uinput,comprintall)){
+				//STUB
+				break;
+			}
+
+			//Quit case
+			if(inArray(uinput,comquit)){
+				break;
+			}
+
+		}
+	}
 
 }
