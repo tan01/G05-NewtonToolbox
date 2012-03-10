608e2faee289deec9b76e471bd4d3c024334bc27
tan01 <fenrir754@gmail.com>
2012-03-10 13:29:43 -0800

    I guess this forms the basis of the GUI.

--- src/gui/NewtonsToolboxGui.java
+++ src/gui/NewtonsToolboxGui.java
@@ -1,27 +1,43 @@
 package gui;
 import javax.swing.*;
+
+import com.sun.awt.AWTUtilities;
+
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Point;
 import java.awt.RenderingHints;
 
 //for textbox?
 import java.awt.TextArea;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 import java.awt.event.WindowAdapter;
+import java.awt.event.MouseMotionAdapter;
+import java.awt.event.MouseAdapter;
+
 
 /**
  * Gui will be the gui used for our Newton's Toolbox project (EDIT DESCRIPTION LATER K THX)
  *  
  * @author Michelle Len
+ * @author Jonathan Tan
+ * @author http://www.java2s.com/Code/Java/Swing-JFC/Createundecoratedframe.htm
  * @version 03/04/2012 for CS 48 Project, W12
  */
 public class NewtonsToolboxGui {
 		
+		//For mouse clicks.
+		private static Point point = new Point();
+	
 		// Declare our private variables
 		private JFrame frame;
 		private NewtonsToolboxPanel panel;
@@ -35,16 +51,53 @@ public class NewtonsToolboxGui {
 	    	// Initialize our frame and set the size
 	    	frame = new JFrame("Newton's Toolbox");
 	    	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE) ;
-	    	frame.setSize(720,480);
+	    	frame.setSize(900,600);
+	    	frame.setUndecorated(true);
 	    	
 	    	// Declaring and creating a panel
 	    	panel = new NewtonsToolboxPanel();
-	 
+	    	
+	    	//THIS IS HOW YOU ADD BUTTONS TO THE BACKGROUND PANEL
+	    	JPanel content = new JPanel(new BorderLayout());
+	    	content.setBounds(780,50,50,50);
+	    	
+	    	JButton quitButton = new JButton("X");
+	    	quitButton.addActionListener(new quitListener());
+	    		
+	    	content.add(quitButton);
+	    	panel.add(content);
+	    	
 	    	// Adding the Border Layout
 	    	frame.getContentPane().add(BorderLayout.CENTER, panel);
-	 
+	    	
+	    	
+	    	//SHAPE
+	    	Quadrilateral blueQuadrilateral = new Quadrilateral();
+    		blueQuadrilateral.addPoint((int)(0.03125*frame.getWidth()), (int)(0.06250*frame.getHeight())); //  1/32(W) &  1/16(H)
+    		blueQuadrilateral.addPoint((int)(0.96875*frame.getWidth()), (int)(0.03125*frame.getHeight())); // 31/32(W) &  1/32(H)
+    		blueQuadrilateral.addPoint((int)(0.90625*frame.getWidth()), (int)(0.96875*frame.getHeight())); // 29/32(W) & 31/32(H)
+    		blueQuadrilateral.addPoint((int)(0.06250*frame.getWidth()), (int)(0.93750*frame.getHeight())); //  1/16(W) & 15/16(H)
+    		
+    		AWTUtilities.setWindowShape(frame, blueQuadrilateral);
+    		
+    		frame.addMouseListener(new MouseAdapter() {
+    		      public void mousePressed(MouseEvent e) {
+    		        point.x = e.getX();
+    		        point.y = e.getY();
+    		      }
+    		    });
+    		
+    		    frame.addMouseMotionListener(new MouseMotionAdapter() {
+    		      public void mouseDragged(MouseEvent e) {
+    		        Point p = frame.getLocation();
+    		        frame.setLocation(p.x + e.getX() - point.x, p.y + e.getY() - point.y);
+    		      }
+    		    });
+	    	
 	    	// Displays the frame
+    		
 	    	frame.setVisible(true);	
+	    	
 	    	
 	    }
 	    
@@ -56,6 +109,9 @@ public class NewtonsToolboxGui {
 	     */
 	    public class NewtonsToolboxPanel extends JPanel {     
 			
+	    	public NewtonsToolboxPanel(){
+	    		super(null);
+	    	}
 	    	// Included to suppress Eclipse Warning
 	    	private static final long serialVersionUID = 1L;
 
@@ -70,9 +126,6 @@ public class NewtonsToolboxGui {
 				// Enable anti-aliasing for smoothing out shapes :)
 	    		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 	    		
-	    		// Draws the White Background
-	    		g2d.setColor(Color.WHITE);
-	    		g2d.fillRect(0, 0, this.getWidth(), this.getHeight());
 	    		
 	    		// Draws the Outer Blue Quadrilateral
 	    		Quadrilateral blueQuadrilateral = new Quadrilateral();
@@ -100,7 +153,15 @@ public class NewtonsToolboxGui {
 	    		g2d.fillRect((int)(0.0937500*this.getWidth()), (int)(0.1250000*this.getHeight()),  //  3/32 (W) &  1/8  (H)
 	    				     (int)(0.6015625*this.getWidth()), (int)(0.1015625*this.getHeight())); // 77/128(W) & 13/128(H)
 	    	}
+	    	
 	    }
+	    
+	    //Button Listener Classes:
+	    class quitListener implements ActionListener {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		}
 	    
 	    // I'm sticking this Main here so I can Ctrl+F11 and view what's drawn
 		public static void main (String[] args) {
