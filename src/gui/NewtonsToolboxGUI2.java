3de142e27e627a200eeec473aa00319f59684cde
maycamp <My Lynn@MayFast.(none)>
2012-03-11 13:12:48 -0700

    alright, scroll is sized correctly now! =D

--- src/gui/NewtonsToolboxGUI2.java
+++ src/gui/NewtonsToolboxGUI2.java
@@ -55,7 +55,7 @@ public class NewtonsToolboxGUI2{
 	JTextField searchBar = new JTextField(50);
 	//JTextArea searchBar = new JTextArea(1,50);
 
-	JTextArea searchResults = new JTextArea(50,57);
+	JTextArea searchResults = new JTextArea(22,57);
 	private JButton searchButton = new JButton("Search");
 	private JButton searchFormsButton = new JButton("Search Formulas");
 	private JButton printFormsButton = new JButton("Print Formulas");
@@ -74,24 +74,18 @@ public class NewtonsToolboxGUI2{
 
 
 		//not needed? setborder thing
-//		middlePanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
-		
-		//wrap words and lines
+		//		middlePanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+
+		//wrap words and lines and make sure you can't edit it
 		searchResults.setLineWrap(true);
 		searchResults.setWrapStyleWord(true);
-
 		searchResults.setEditable(false);
-		
+
 		//scroller
 		scroller = new JScrollPane(searchResults);
 		scroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 		scroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 
-				scroller.setBounds(3, 3, 300, 200);
-
-
-
-		searchResults.setRows(5);
 		//setting layout of panels
 		frame.getContentPane().add(BorderLayout.NORTH, topPanel);
 		frame.getContentPane().add(BorderLayout.CENTER, middlePanel);
@@ -113,7 +107,7 @@ public class NewtonsToolboxGUI2{
 		topPanel.add(searchBar);
 		topPanel.add(searchButton);
 
-		//searchResults was added to scroller
+		//searchResults was added to scroller, so DO NOT add to panel
 		middlePanel.add(scroller);
 
 		bottomPanel.add(searchFormsButton);
@@ -121,62 +115,48 @@ public class NewtonsToolboxGUI2{
 		bottomPanel.add(solveFormsButton);
 		bottomPanel.add(addFormsButton);
 
-
-
-
 		searchButton.addActionListener(new searchButtonListener());
 		frame.setVisible(true);
+	}
 
+	//THIS SHOULD BE IN THE GUISEARCH MODULE
+	public void printSearchToTextArea(){
+		String userInput = searchBar.getText();
+		FormulaDatabase defaultFormulas = (FormulaDatabase)Saver.loadForms();
+		Search searchObject = new Search(defaultFormulas);
+		ArrayList<Formula> foundFormulas = searchObject.searchF(userInput);
 
+		String stringOfFormulas = "";
+
+		for(int i=0; i<foundFormulas.size();i++) {
+			stringOfFormulas = stringOfFormulas + foundFormulas.get(i).allInfoToString() + "\n \n";
+		}
+
+		searchResults.setText("You searched for: " + userInput + "\n" +
+				"Found " + foundFormulas.size() + " formulas:\n\n" +
+				stringOfFormulas);
+		//Search is done, clears the search bar.
+		searchBar.setText("");
+	}
+
+	public class NewtonsToolboxPanel extends JPanel {
+		// Included to suppress Eclipse Warning
+		private static final long serialVersionUID = 1L;
+	}
+
+	//Button Listener Classes:
+	class searchButtonListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			printSearchToTextArea();
+		}
+	}
+
+	public static void main(String[] args) {
+
+		NewtonsToolboxGUI2 gui = new NewtonsToolboxGUI2();
+		gui.go();
 	}
 
 
-		//THIS SHOULD BE IN THE GUISEARCH MODULE
-		public void printSearchToTextArea(){
-			String userInput = searchBar.getText();
-			FormulaDatabase defaultFormulas = (FormulaDatabase)Saver.loadForms();
-			Search searchObject = new Search(defaultFormulas);
-			ArrayList<Formula> foundFormulas = searchObject.searchF(userInput);
 
-			String stringOfFormulas = "";
-
-
-
-
-
-			for(int i=0; i<foundFormulas.size();i++) {
-				stringOfFormulas = stringOfFormulas + foundFormulas.get(i).allInfoToString() + "\n \n";
-			}
-
-			searchResults.setText("You searched for: " + userInput + "\n" +
-					"Found " + foundFormulas.size() + " formulas:\n\n" +
-					stringOfFormulas);
-			//Search is done, clears the search bar.
-			searchBar.setText("");
-		}
-
-		public class NewtonsToolboxPanel extends JPanel {
-			// Included to suppress Eclipse Warning
-			private static final long serialVersionUID = 1L;
-
-
-
-		}
-
-		//Button Listener Classes:
-		class searchButtonListener implements ActionListener {
-			public void actionPerformed(ActionEvent e) {
-				printSearchToTextArea();
-			}
-		}
-		
-		
-		public static void main(String[] args) {
-
-			NewtonsToolboxGUI2 gui = new NewtonsToolboxGUI2();
-			gui.go();
-		}
-
-
-
-	}
+}
