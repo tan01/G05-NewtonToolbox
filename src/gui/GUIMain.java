07a3df574d744a2480f072bdecc74e4d46ce9034
Cronomaru <Clayven Anderson@ClayvenAnderson.(none)>
2012-03-11 15:18:42 -0700

    -whitespace
    Signed-off-by: Cronomaru <Clayven Anderson@ClayvenAnderson.(none)>

--- src/gui/GUIMain.java
+++ src/gui/GUIMain.java
@@ -19,6 +19,12 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+
+
+
+
+
+
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 import java.awt.event.WindowAdapter;
