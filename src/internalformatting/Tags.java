c79f1ce0e6eced500fc0ab664e8f5af41c776357
maycamp <My Lynn@MayFast.(none)>
2012-03-15 23:28:55 -0700

    got rid of commas at the end of commas

--- src/internalformatting/Tags.java
+++ src/internalformatting/Tags.java
@@ -113,7 +113,7 @@ public class Tags extends ArrayList<String> implements Serializable{
 		else {
 			for(int i=0; i<this.size(); i++){
 				AllTags = AllTags + this.get(i); //***TODO format this a bit better***
-				if(!(i==this.size())) {
+				if(!(i==this.size()-1)) {
 					AllTags = AllTags + ",";
 				}
 			}
