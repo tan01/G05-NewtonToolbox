de7614f3fcb6d0525f97e640d533d0b8f1bc55be
Michelle Len <mlen@umail.ucsb.edu>
2012-03-15 22:20:21 -0700

    Fixed indentation and spacing.
    Signed-off-by: Michelle Len <mlen@umail.ucsb.edu>

--- src/userinterface/CLIPrint.java
+++ src/userinterface/CLIPrint.java
@@ -8,21 +8,21 @@ package userinterface;
 			Not sure exactly how I'm supposed to do this.
 		@author Jonathan Tan
 		@version 0.0.1 2/21/12
-*/
+ */
 
 //Does this even deserve to be a class?
 //Will this eventually get more complicated?
 
-public class CLIPrint extends CLI{
-	
-	static public void printAll(){
-		for(int i=0;i<defaultFormulas.getSize();i++){
+public class CLIPrint extends CLI {
+
+	static public void printAll() {
+		for(int i=0; i<defaultFormulas.getSize(); i++) {
 			defaultFormulas.get(i).printFormula();
 			System.out.println();
 		}
 	}
-	
+
 	//Will probably be a single "print" method when I can think of a way to make an adequate
 	//"Database Explorer."
-	
+
 }
